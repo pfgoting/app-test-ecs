44221c0e92e88da357e3fa4a0862a98c7bf04ced
pfgoting <pfgoting@gmail.com>
2021-09-17 20:02:37 +0800

    Added health endpoint

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 
+@app.route('/')
+def index():
+    return "index"
+
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
     """Reverse and return the provided URI."""
@@ -16,4 +20,4 @@ def returnBackwardsString(random_string):
     return "".join(reversed(random_string))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
